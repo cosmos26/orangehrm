c183e5a4a1293aa608044cf47d6efc14c1c6ed1b
kiraz26 <halilkirazz@gmail.com>
2019-06-25 13:02:09 -0400

    Cukes Runner glue path is changed

--- src/test/java/com/app/orangehrm/stepdefinitions/Login_Step_Definition.java
+++ src/test/java/com/app/orangehrm/stepdefinitions/Login_Step_Definition.java
@@ -1,5 +1,25 @@
 package com.app.orangehrm.stepdefinitions;
 
-public class Login_Step_Definition {
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
+public class Login_Step_Definition {
+	@Given("dfdfdf")
+	public void dfdfdf() {
+	    // Write code here that turns the phrase above into concrete actions
+	   
+	}
+
+	@When("sdkdlfj")
+	public void sdkdlfj() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("gggg")
+	public void gggg() {
+	    // Write code here that turns the phrase above into concrete actions
+	   
+	}
 }
