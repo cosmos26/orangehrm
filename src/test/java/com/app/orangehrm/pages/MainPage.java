97f62b66395ab8303e5945b9a1cb5c4c567ee0ff
kiraz26 <halilkirazz@gmail.com>
2019-07-21 20:47:39 -0400

    URL and some locaters are changed

--- src/test/java/com/app/orangehrm/pages/MainPage.java
+++ src/test/java/com/app/orangehrm/pages/MainPage.java
@@ -15,10 +15,10 @@ public class MainPage {
 		PageFactory.initElements(driver, this);
 	}
 	
-	@FindBy(xpath="//span[contains(text(),'PIM')]")
+	@FindBy(xpath="//*[@id=\"menu_pim_viewPimModule\"]")
 	public WebElement pimButton;
 	
-	@FindBy(xpath="//a[@id='menu_pim_addEmployee']//span[@class='left-menu-title'][contains(text(),'Add Employee')]")
+	@FindBy(xpath="//*[@id=\"menu_pim_addEmployee\"]")
 	public WebElement addEmployeeButton;
 	
 }
--- src/test/java/com/app/orangehrm/stepdefinitions/Add_Employee_Step_Definition.java
+++ src/test/java/com/app/orangehrm/stepdefinitions/Add_Employee_Step_Definition.java
@@ -5,15 +5,17 @@ import static org.testng.Assert.assertEquals;
 import com.app.orangehrm.pages.MainPage;
 import com.app.orangehrm.pages.WindowAddEmployeePage;
 import com.app.orangehrm.utilities.BrowserUtils;
+import com.app.orangehrm.utilities.ConfigurationReader;
 import com.app.orangehrm.utilities.Driver;
 
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Add_Employee_Step_Definition {
 	MainPage mainPage = new MainPage();
-	WindowAddEmployeePage addEmployeePage= new WindowAddEmployeePage();
-	
+	WindowAddEmployeePage addEmployeePage = new WindowAddEmployeePage();
+
 	@Then("User click PIM button")
 	public void user_click_PIM_button() {
 		mainPage.pimButton.click();
@@ -26,13 +28,13 @@ public class Add_Employee_Step_Definition {
 
 	@Then("User enter employee data {string} {string} and location")
 	public void user_enter_employee_data_and_location(String string, String string2) {
-	    BrowserUtils.waitForVisibility(addEmployeePage.firstName, 10);
+		BrowserUtils.waitForVisibility(addEmployeePage.firstName, 10);
 		addEmployeePage.firstName.sendKeys(string);
-		
-	    addEmployeePage.lastName.sendKeys(string2);
-	    addEmployeePage.locationDropDown.click();
-	    addEmployeePage.newYorkSalesOffice.click();
-	    
+
+		addEmployeePage.lastName.sendKeys(string2);
+		addEmployeePage.locationDropDown.click();
+		addEmployeePage.newYorkSalesOffice.click();
+
 	}
 
 	@Then("User click next button")
@@ -41,7 +43,7 @@ public class Add_Employee_Step_Definition {
 		BrowserUtils.waitFor(3);
 		addEmployeePage.eeoRaceAndEthnicityDropDown.click();
 		addEmployeePage.white.click();
-		
+
 		BrowserUtils.waitForClickablility(addEmployeePage.nextButton2, 10);
 		addEmployeePage.nextButton2.click();
 	}
@@ -51,10 +53,10 @@ public class Add_Employee_Step_Definition {
 		BrowserUtils.waitFor(5);
 		addEmployeePage.regionDropDown.click();
 		addEmployeePage.region_1.click();
-		
+
 		addEmployeePage.fteDropDown.click();
 		addEmployeePage.fte0_5.click();
-		
+
 		addEmployeePage.temporaryDepartmentDropDown.click();
 		addEmployeePage.subUnit_1.click();
 	}
@@ -62,16 +64,14 @@ public class Add_Employee_Step_Definition {
 	@When("User click save button")
 	public void user_click_save_button() {
 		addEmployeePage.saveButton.click();
-		
+
 	}
 
 	@Then("Employee's Personal Details page should be loaded")
 	public void employee_s_Personal_Details_page_should_be_loaded() {
 		BrowserUtils.waitForVisibility(addEmployeePage.personalDetailsTab, 5);
 		assertEquals(addEmployeePage.expectedPersonalDetailsPageTitle, Driver.getDriver().getTitle());
-		System.out.println("Actual page title is: "+Driver.getDriver().getTitle());
+		System.out.println("Actual page title is: " + Driver.getDriver().getTitle());
 	}
-
-	
 
 }
