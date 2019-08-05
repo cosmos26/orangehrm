3d2cd140a1dafa0245496a891ee87a86d6a966d4
kiraz26 <halilkirazz@gmail.com>
2019-08-05 15:04:16 -0400

    Smoke and Regression Suite are added

---
+++ src/test/java/com/app/orangehrm/runners/RegressionRunner.java
@@ -0,0 +1,16 @@
+package com.app.orangehrm.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(plugin = { "pretty", "html:target/cucumber-report",
+		"json:target/cucumber.json", "rerun:target/rerun.txt" }, 
+		tags = "@regression",
+		features = { "src/test/resources/com/orangehrm/features" }, 
+		glue = "com/app/orangehrm/stepdefinitions",
+		dryRun = false
+		
+)
+public class RegressionRunner extends AbstractTestNGCucumberTests {
+
+}
