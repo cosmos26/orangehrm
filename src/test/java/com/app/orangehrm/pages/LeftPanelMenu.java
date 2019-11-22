e488b0d230980ff4a90c8e8f7208f87c05fd7dcb
Bek Kenchenbayev <iskk@yahoo.com>
2019-11-22 09:43:29 -0500

    Update locators pages/DirectDepositAdd.java

--- src/test/java/com/app/orangehrm/pages/LeftPanelMenu.java
+++ src/test/java/com/app/orangehrm/pages/LeftPanelMenu.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class LeftPanelMenu {
+	
 	public static WebElement element = null;
 	
 	public static WebElement itemPIM (WebDriver driver) {
